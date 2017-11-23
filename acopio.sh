7120ff95443f3d4879132470ae66f0b0b42ab534
Dayana Aguire Iniguez <daiaguirrei@correo.ugr.es>
2017-11-23 23:36:28 +0100

    Cierro isuue close #10

--- acopio.sh
+++ acopio.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+#Creamos el grupo de recursos
+echo Creando maquina virtual - vmk
+az group create --name CCdaivmk1 --location westus
+
+# Comando para crear maquina virtual 
+ip=$(az vm create --resource-group CCdaivmk1 --name HitoCC1 --image UbuntuLTS --generate-ssh-keys | jq -r '.publicIpAddress')
+echo -ip: $ip
+
+# realizando el provisionamiento
+ansible-playbook -i "$ip," webserver.yml -u dayanna
